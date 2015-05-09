4225b7586835de140eb335ff45ee07878bbeef50
pvalienteverde <pvalienteverde@gmail.com>
2015-05-09 13:18:11 +0200

    Se añade ejemplo de como realizar conversores en ambas direcciones

---
+++ python_boost/Release/scr/recubrimiento_clases.d
@@ -0,0 +1,16 @@
+scr/recubrimiento_clases.d: ../scr/recubrimiento_clases.cpp \
+ /home/pedro/BoostPython/utiles/include/SerieHistorica.h \
+ /home/pedro/BoostPython/utiles/include/matematicas/constantes.h \
+ /home/pedro/BoostPython/utiles/include/matematicas/constantes.h \
+ /home/pedro/BoostPython/utiles/include/fechas/fechas.h \
+ /usr/include/python2.7/datetime.h
+
+/home/pedro/BoostPython/utiles/include/SerieHistorica.h:
+
+/home/pedro/BoostPython/utiles/include/matematicas/constantes.h:
+
+/home/pedro/BoostPython/utiles/include/matematicas/constantes.h:
+
+/home/pedro/BoostPython/utiles/include/fechas/fechas.h:
+
+/usr/include/python2.7/datetime.h:
